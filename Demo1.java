bdbd04c3a98d3930f6da6441ba912df692f94e92
olu4edureka <75208552+olu4edureka@users.noreply.github.com>
2020-11-29 14:07:41 +0000

    Update file on remote

--- Demo1.java
+++ Demo1.java
@@ -2,6 +2,7 @@ class Demo1
 {
 	public static void maain(String args[]){
 	System.out.println("This is Java class Demo1");
+	System.out.println("This is second line remote from Java class Demo1");
 	}
 
 }
